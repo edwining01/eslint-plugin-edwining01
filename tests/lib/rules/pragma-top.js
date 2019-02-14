1e2b3ffe58edaf2dc257e2dfb239bb535cbce6d4
Edwin Ting <edwining01@users.noreply.github.com>
2019-02-14 21:01:50 +0800

    New rule `pragma-top`

---
+++ tests/lib/rules/pragma-top.js
@@ -0,0 +1,46 @@
+/**
+ * @fileoverview Force import uses single quote
+ * @author Edwin Ting <40881695+edwining01@users.noreply.github.com>
+ */
+"use strict"
+
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+const rule = require('../../../lib/rules/pragma-top')
+const RuleTester = require('eslint').RuleTester
+const DEFAULT_PRAGMA_COMMENT_STR = "/** @format */"
+const loc = 0
+//------------------------------------------------------------------------------
+// Tests
+//------------------------------------------------------------------------------
+const ruleTester = new RuleTester()
+
+ruleTester.run("pragma-top", rule, {
+  valid: [
+    DEFAULT_PRAGMA_COMMENT_STR,
+    { code: "/* @format */", options: [{ pragmaCommentStr: "/* @format */" }] },
+    { code: "/** @customPragma */", options: [{
+      pragmaStr: "@customPragma", pragmaCommentStr: "/** @customPragma */" }] },
+    { code: "// @format", options: [{ pragmaCommentStr: "// @format" }] },
+  ],
+  invalid: [
+    {
+      code: "",
+      errors: [
+        {
+          loc,
+          message: "Expected pragma comment `/** @format */` at the top of file.",
+        }
+      ],
+      output: DEFAULT_PRAGMA_COMMENT_STR,
+    },
+    {
+      code: "/**  @format */",
+      errors: [{
+        message: "Expected top pragma comment to be `/** @format */`.",
+      }],
+      output: DEFAULT_PRAGMA_COMMENT_STR,
+    },
+  ]
+})
