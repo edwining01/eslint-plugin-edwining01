f7a6bbd0fabe69b456b6ce2a8b9f92ce1ded29ee
Edwin Ting <edwining01@users.noreply.github.com>
2020-09-10 18:05:13 +0800

    Try to add TemplateLiteral

--- tests/lib/rules/force-single-quote.js
+++ tests/lib/rules/force-single-quote.js
@@ -42,92 +42,99 @@ const errorExportAll = {
 
 ruleTester.run("import-single-quote", rule, {
   valid: [
-    // import
-    {
-      code: `import 'hello-world'`,
-      parserOptions,
-    },
-    {
-      code: `import myVar from 'hello-world'`,
-      parserOptions,
-    },
-    {
-      code: `import { myVar1, myVar2 } from 'hello-world'`,
-      parserOptions,
-    },
-    {
-      code: `import myVar0, { myVar1, myVar2 } from 'hello-world'`,
-      parserOptions,
-    },
-    {
-      code: `import * as myVar from 'hello-world'`,
-      parserOptions,
-    },
-  
-    // dynamic import
-    {
-      code: `import('hello-world')`,
-      parser,
-      parserOptions,
-    },
-  
-    // require
-    {
-      code: `require('hello-world')`,
-      parserOptions,
-    },
-    
-    // export
-    {
-      code: `export * from 'hello-world'`,
-      parserOptions,
-    },
-    {
-      code: `export { abc } from 'hello-world'`,
-      parserOptions,
-    },
-    {
-      code: `export { abc as defg } from 'hello-world'`,
-      parserOptions,
-    },
+  //   // import
+    // {
+    //   code: `import 'hello-world'`,
+    //   parserOptions,
+    // },
+  //   {
+  //     code: `import myVar from 'hello-world'`,
+  //     parserOptions,
+  //   },
+  //   {
+  //     code: `import { myVar1, myVar2 } from 'hello-world'`,
+  //     parserOptions,
+  //   },
+  //   {
+  //     code: `import myVar0, { myVar1, myVar2 } from 'hello-world'`,
+  //     parserOptions,
+  //   },
+  //   {
+  //     code: `import * as myVar from 'hello-world'`,
+  //     parserOptions,
+  //   },
+  // 
+  //   // dynamic import
+  //   {
+  //     code: `import('hello-world')`,
+  //     parser,
+  //     parserOptions,
+  //   },
+  // 
+  //   // require
+  //   {
+  //     code: `require('hello-world')`,
+  //     parserOptions,
+  //   },
+  // 
+  //   // export
+  //   {
+  //     code: `export * from 'hello-world'`,
+  //     parserOptions,
+  //   },
+  //   {
+  //     code: `export { abc } from 'hello-world'`,
+  //     parserOptions,
+  //   },
+  //   {
+  //     code: `export { abc as defg } from 'hello-world'`,
+  //     parserOptions,
+  //   },
   ],
   
   invalid: [
-    // import
-    {
-      code: "import \"hello-world\"",
-      errors: [errorImport],
-      output: "import 'hello-world'",
-      parserOptions,
-    },
-    {
-      code: `import myVar from "hello-world"`,
-      errors: [errorImport],
-      output: `import myVar from 'hello-world'`,
-      parserOptions,
-    },
-    {
-      code: `import { myVar1, myVar2 } from "hello-world"`,
-      errors: [errorImport],
-      output: `import { myVar1, myVar2 } from 'hello-world'`,
-      parserOptions,
-    },
-    {
-      code: `import myVar0, { myVar1, myVar2 } from "hello-world"`,
-      errors: [errorImport],
-      output: `import myVar0, { myVar1, myVar2 } from 'hello-world'`,
-      parserOptions,
-    },
-    {
-      code: `import * as myVar from "hello-world"`,
-      errors: [errorImport],
-      output: `import * as myVar from 'hello-world'`,
-      parserOptions,
-    },
+  //   // import
+    // {
+    //   code: "import \"hello-world\"",
+    //   errors: [errorImport],
+    //   output: "import 'hello-world'",
+    //   parserOptions,
+    // },
+  //   {
+  //     code: `import myVar from "hello-world"`,
+  //     errors: [errorImport],
+  //     output: `import myVar from 'hello-world'`,
+  //     parserOptions,
+  //   },
+  //   {
+  //     code: `import { myVar1, myVar2 } from "hello-world"`,
+  //     errors: [errorImport],
+  //     output: `import { myVar1, myVar2 } from 'hello-world'`,
+  //     parserOptions,
+  //   },
+  //   {
+  //     code: `import myVar0, { myVar1, myVar2 } from "hello-world"`,
+  //     errors: [errorImport],
+  //     output: `import myVar0, { myVar1, myVar2 } from 'hello-world'`,
+  //     parserOptions,
+  //   },
+  //   {
+  //     code: `import * as myVar from "hello-world"`,
+  //     errors: [errorImport],
+  //     output: `import * as myVar from 'hello-world'`,
+  //     parserOptions,
+  //   },
     
     // dynamic import
+    // {
+    //   code: `import("hello-world")`,
+    //   errors: [errorDynamicImport],
+    //   output: `import('hello-world')`,
+    //   parser,
+    //   parserOptions,
+    // },
     {
-      code: `import("hello-world")`,
+      code: `import(\`hello-world\`)`,
       errors: [errorDynamicImport],
       output: `import('hello-world')`,
       parser,
@@ -135,31 +142,37 @@ ruleTester.run("import-single-quote", rule, {
     },
     
     // // require
+    // {
+    //   code: `require("hello-world")`,
+    //   errors: [errorRequire],
+    //   output: `require('hello-world')`,
+    //   parserOptions,
+    // },
     {
-      code: `require("hello-world")`,
+      code: `require(\`cssnano\`)`,
       errors: [errorRequire],
-      output: `require('hello-world')`,
+      output: `require('cssnano')`,
       parserOptions,
     },
     
     // export
-    {
-      code: `export * from "hello-world"`,
-      errors: [errorExportAll],
-      output: `export * from 'hello-world'`,
-      parserOptions,
-    },
-    {
-      code: `export { abc } from "hello-world"`,
-      errors: [errorExportNamed],
-      output: `export { abc } from 'hello-world'`,
-      parserOptions,
-    },
-    {
-      code: `export { abc as defg } from "hello-world"`,
-      errors: [errorExportNamed],
-      output: `export { abc as defg } from 'hello-world'`,
-      parserOptions,
-    },
+    // {
+    //   code: `export * from "hello-world"`,
+    //   errors: [errorExportAll],
+    //   output: `export * from 'hello-world'`,
+    //   parserOptions,
+    // },
+    // {
+    //   code: `export { abc } from "hello-world"`,
+    //   errors: [errorExportNamed],
+    //   output: `export { abc } from 'hello-world'`,
+    //   parserOptions,
+    // },
+    // {
+    //   code: `export { abc as defg } from "hello-world"`,
+    //   errors: [errorExportNamed],
+    //   output: `export { abc as defg } from 'hello-world'`,
+    //   parserOptions,
+    // },
   ]
 })
